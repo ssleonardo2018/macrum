db9c8c2cc40f961c4ce90d4483372db0c7d807fe
ssleonardo2018 <115373834+ssleonardo2018@users.noreply.github.com>
2026-04-28 00:57:37 -0300

    Update apiUsuarios.js

--- js/apiUsuarios.js
+++ js/apiUsuarios.js
@@ -3,7 +3,7 @@ const API = (() => {
     const usuarios = [
         { nome: "Admin", email: "admin@macrum.com", senha: "123", role: "admin" },
         { nome: "João", email: "paciente@mail.com", senha: "123", role: "paciente" },
-        { nome: "Maria", email: "nutri@mail.com", senha: "123", role: "nutricionista" }
+        { nome: "Maria", email: "nutri@macrum.com", senha: "123", role: "nutricionista" }
     ];
 
     return {
